496de7f7280352826bc556a71e4a69227f920132
casdev2020 <87313911+casdev2020@users.noreply.github.com>
2026-07-08 10:39:25 +0100

    chore(main): release 2.0.1 (#26)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="gitlab-compliance",
-    version="2.0.0",
+    version="2.0.1",
     description="a tools that generates markdown documentation from gitlab yaml",
     license="MIT",
     packages=["gitlab-compliance"],
